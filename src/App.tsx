b08e1fb5a99caa7d14ceb40fb4f299c7d2098cf2
Nakajima <kituneudon97@gmail.com>
2023-09-16 16:25:20 +0900

    3 develop stopwatch (#6)
    * #3 ボタンをクリックした際にStartとStopが交互に表示されるようにした
    
    * #3 ストップボタンの色を変更
    
    * #3 startを押した際には時間が進み、stopを押した際には時間が止まる処理を追加
    
    * #3 クリアボタンの追加
    
    * #3 見た目を整えた

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,87 @@
+import { useEffect, useState } from 'react';
 import { Typography, Button, Container, Box } from '@mui/material';
 
 const App = (): JSX.Element => {
+  const [isRunningStopwatch, setIsRunningStopwatch] = useState(false);
+  const [time, setTime] = useState(0);
+
+  const handleStartStopwatch = () => {
+    setIsRunningStopwatch(true);
+  };
+
+  const handlePauseStopwatch = () => {
+    setIsRunningStopwatch(false);
+  };
+
+  const handleClearTimer = () => {
+    setTime(0);
+  };
+
+  useEffect(() => {
+    let interval: NodeJS.Timer;
+
+    if (isRunningStopwatch) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isRunningStopwatch]);
+
+  const formatTimer = (time: number) => {
+    const hour = Math.floor(time / 3600);
+    const minute = Math.floor((time - hour * 3600) / 60);
+    const second = time % 60;
+
+    const formattedHours = String(hour).padStart(2, '0');
+    const formattedMinute = String(minute).padStart(2, '0');
+    const formattedSecond = String(second).padStart(2, '0');
+
+    return `${formattedHours}:${formattedMinute}:${formattedSecond}`;
+  };
+
   return (
     <>
       <Container>
         <Box>
-          <Typography>00:00:00</Typography>
+          <Typography>{formatTimer(time)}</Typography>
         </Box>
-        <Button variant="contained">Start</Button>
+        <Box sx={{ display: 'flex', gap: '5px' }}>
+          {isRunningStopwatch ? (
+            <Button
+              variant="contained"
+              color="error"
+              onClick={() => {
+                handlePauseStopwatch();
+              }}
+            >
+              Stop
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              onClick={() => {
+                handleStartStopwatch();
+              }}
+            >
+              {time === 0 ? 'Start' : 'Restart'}
+            </Button>
+          )}
+          {time !== 0 && !isRunningStopwatch && (
+            <Button
+              variant="contained"
+              color="success"
+              onClick={() => {
+                handleClearTimer();
+              }}
+            >
+              Clear
+            </Button>
+          )}
+        </Box>
       </Container>
     </>
   );
