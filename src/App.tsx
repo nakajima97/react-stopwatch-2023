9a27717fb94cad62745a198aa23be98e35675c25
Yudai Nakajima <kituneudon97@gmail.com>
2023-09-16 14:46:26 +0900

    #2 静的なバージョンのアプリを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { Typography, Button, Container, Box } from '@mui/material';
 
-function App() {
-  return <></>;
-}
+const App = (): JSX.Element => {
+  return (
+    <>
+      <Container>
+        <Box>
+          <Typography>00:00:00</Typography>
+        </Box>
+        <Button variant="contained">Start</Button>
+      </Container>
+    </>
+  );
+};
 
 export default App;
