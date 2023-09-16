e1968e9480e3fdce74309a295f0639f7bf372261
Yudai Nakajima <kituneudon97@gmail.com>
2023-09-16 15:05:53 +0900

    #3 ボタンをクリックした際にStartとStopが交互に表示されるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
+import { useState } from 'react';
 import { Typography, Button, Container, Box } from '@mui/material';
 
 const App = (): JSX.Element => {
+  const [isRunningStopwatch, setIsRunningStopwatch] = useState(false);
+
+  const handleStartStopwatch = () => {
+    setIsRunningStopwatch(true);
+  };
+
+  const handlePauseStopwatch = () => {
+    setIsRunningStopwatch(false);
+  };
+
   return (
     <>
       <Container>
         <Box>
           <Typography>00:00:00</Typography>
         </Box>
-        <Button variant="contained">Start</Button>
+        {isRunningStopwatch ? (
+          <Button
+            variant="contained"
+            onClick={() => {
+              handlePauseStopwatch();
+            }}
+          >
+            Stop
+          </Button>
+        ) : (
+          <Button
+            variant="contained"
+            onClick={() => {
+              handleStartStopwatch();
+            }}
+          >
+            Start
+          </Button>
+        )}
       </Container>
     </>
   );
